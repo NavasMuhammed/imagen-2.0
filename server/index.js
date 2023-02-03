4d3e911713e3982f6dcb65c16103136bfe474d65
NavasMuhammed <navas3444a3@gmail.com>
2023-02-03 20:29:37 +0530

    init repo

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import imagenRoutes from "./routes/imagenRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }))
+
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/imagen', imagenRoutes)
+
+app.get('/', async (req, res) => {
+    res.send('Hello from ImaGens!')
+});
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('serve has started on port http://localhost:8080'))
+    } catch (error) {
+        console.log(error);
+    }
+}
+startServer();
